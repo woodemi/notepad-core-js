b0f27ade2d65e53c938dcaa6e64b73161ce7a595
wangkun <wangkun@woodemi.com>
2019-12-31 14:34:43 +0800

    Add `connect` & `disconnect` to `NotepadConnector`
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- example.js
+++ example.js
@@ -6,3 +6,12 @@ window.requestDevice = async function () {
     window.notepad = await notepadConnector.requestDevice();
     console.log('requestDevice', window.notepad);
 };
+
+window.connect = function (device) {
+    notepadConnector.connect(device);
+};
+
+window.disconnect = function () {
+    console.log('notepadConnector.connectGatt', notepadConnector.connectGatt);
+    notepadConnector.disconnect();
+};
--- src/NotepadConnector.js
+++ src/NotepadConnector.js
@@ -4,6 +4,22 @@ class NotepadConnector {
             acceptAllDevices: true,
         });
     }
+
+    #connectGatt;
+
+    async connect(device) {
+        try {
+            this.#connectGatt = await device.gatt.connect();
+            console.log(`onConnectSuccess ${(this.#connectGatt)}, ${this.#connectGatt.connected}`);
+        } catch (e) {
+            console.log('onConnectFail');
+        }
+    }
+
+    disconnect() {
+        if (this.#connectGatt)
+            this.#connectGatt.disconnect();
+    }
 }
 
 export default NotepadConnector;
