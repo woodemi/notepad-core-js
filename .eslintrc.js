9c2196c79d7ced065811fbfb4c6f50f65bc43ffa
Jing Hong Gang <honggangjing@gmail.com>
2020-05-10 20:14:55 +0800

    Add example/web

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
     "extends": "eslint:recommended",
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
+        "module": true,
+        "require": true
     },
     "parser": "babel-eslint",
     "parserOptions": {
