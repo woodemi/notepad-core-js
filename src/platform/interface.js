566131095a460fcd6364a0a9c396e9500ade1e59
wangkun <wangkun@woodemi.com>
2020-05-12 17:45:45 +0800

    Fix env condition for `platform-interface`

--- src/platform/interface.js
+++ src/platform/interface.js
@@ -1,4 +1,4 @@
 import { web } from "./web.js";
 import { miniWechat } from "./mini-wechat.js";
 
-export const notepadCore = navigator.userAgent.match(/MicroMessenger/i) ? miniWechat : web;
+export const notepadCore = wx ? miniWechat : web;
