1f8839037592f6cc7a69cedbb69e8cc2eda26ec7
wangkun <wangkun@woodemi.com>
2020-05-12 15:47:22 +0800

    Refactor web example with `requestDevice`

--- example/web/pages/index.js
+++ example/web/pages/index.js
@@ -1,70 +1,14 @@
-import Head from 'next/head';
-const {NotepadConnectionState, NotepadConnector} = require("../../../lib/index");
-const {NotepadMode} = require("../../../lib/models");
-const notepadConnector = new NotepadConnector();
+import Head from "next/head";
+
+let notepadConnector;
+
+import("notepad-core").then(({ NotepadConnector }) => {
+   notepadConnector = new NotepadConnector();
+});
 
 async function bindRequestDevice() {
-    this.device = await notepadConnector.requestDevice();
-    console.log("requestDevice", this.device);
+    await notepadConnector.requestDevice();
 }
-
-function bindConnect() {
-    notepadConnector.connect(this.device);
-}
-
-function bindDisconnect() {
-    notepadConnector.disconnect();
-}
-
-function bindSetMode() {
-    this.notepadClient.setMode(NotepadMode.Sync);
-}
-
-const getCanvas = function () {
-    if (!this.canvas) {
-        this.canvas = document.getElementById("canvas");
-    }
-    return this.canvas;
-};
-
-const getScaleRatio = function () {
-    if (!this.scaleRatio) {
-        let rect = getCanvas().getBoundingClientRect();
-        this.scaleRatio = Math.min(rect.width / 14800.0, rect.height / 21000.0);
-    }
-    return this.scaleRatio;
-};
-
-const handleSyncPointer = function (pointers) {
-    let context = getCanvas().getContext("2d");
-    let scaleRatio = getScaleRatio();
-    for (let p of pointers) {
-        let pre = this.prePointer ? this.prePointer.p : 0;
-        if (pre <= 0 && p.p > 0) {
-            context.beginPath();
-            context.moveTo(p.x * scaleRatio, p.y * scaleRatio);
-        } else if (pre > 0 && p.p > 0) {
-            context.lineTo(p.x * scaleRatio, p.y * scaleRatio);
-        } else if (pre > 0 && p.p <= 0) {
-            context.stroke();
-        }
-        this.prePointer = p;
-    }
-};
-
-notepadConnector.connectionChangeHandler = function (client, state) {
-    console.log(`handleConnectionChange ${client}, ${state}`);
-    switch (state) {
-        case NotepadConnectionState.disconnected:
-            if (this.notepadClient) this.notepadClient.syncPointerHandler = null;
-            this.notepadClient = null;
-            break;
-        case NotepadConnectionState.connected:
-            this.notepadClient = client;
-            this.notepadClient.syncPointerHandler = handleSyncPointer;
-            break;
-    }
-};
 
 export default function Home() {
     return (
@@ -76,12 +20,7 @@ export default function Home() {
           <main>
               <div className="grid">
                   <button onClick={bindRequestDevice}>RequestDevice</button>
-                  <button onClick={bindConnect}>Connect</button>
-                  <button onClick={bindDisconnect}>Disconnect</button>
-                  <button onClick={bindSetMode}>setMode</button>
               </div>
-              画布：
-              <canvas id="canvas" className="canvas"/>
           </main>
           <style jsx>{`
         .container {
@@ -102,12 +41,6 @@ export default function Home() {
           align-items: center;
         }
         
-        canvas {
-          width: 444px;
-          height: 630px;
-          background-color: #FEFEFE
-        }
-
         .grid {
           display: flex;
           align-items: center;
