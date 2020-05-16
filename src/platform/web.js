7f0c3a6a06e55b4118919f6a07b278e88bb62fb2
wangkun <wangkun@woodemi.com>
2020-05-16 20:01:30 +0800

    Impl internal API

--- src/platform/web.js
+++ src/platform/web.js
@@ -1,3 +1,10 @@
+const disconnectEvent = "gattserverdisconnected";
+
+async function getCharacteristic(gatt, { serviceId, characteristicId }) {
+  const service = await gatt.getPrimaryService(serviceId);
+  return await service.getCharacteristic(characteristicId);
+}
+
 export default class NotepadCore {
   requestDevice(scanOptions) {
     return navigator.bluetooth.requestDevice({
@@ -12,5 +19,56 @@ export default class NotepadCore {
 
   stopScan() {
     throw new Error("Unsupported");
+  }
+
+  connect(scanResult) {
+    console.info("NotepadCore connect");
+    let that = this;
+    scanResult.gatt.connect().then((result) => {
+      console.debug("onConnectSuccess", result);
+      that._connectGatt = result;
+      that._connectGatt.device.addEventListener(disconnectEvent, this._handleDisconnectEvent.bind(this));
+      // Connected
+    }, (error) => {
+      console.debug("onConnectFail", error);
+      // Disconnected
+    });
+    // Connecting
+  }
+
+  disconnect() {
+    console.info("NotepadCore disconnect");
+    this._connectGatt.device.removeEventListener(disconnectEvent, this._handleDisconnectEvent);
+    this.deviceId = null;
+  }
+
+  _handleDisconnectEvent(event) {
+    console.debug("_handleDisconnectEvent", event.target.hashCode);
+
+    this._connectGatt.device.removeEventListener(disconnectEvent, this._handleDisconnectEvent);
+    // Disconnected
+  }
+
+  async setNotifiable({ serviceId, characteristicId }, state) {
+    const characteristic = await getCharacteristic(this._connectGatt, { serviceId, characteristicId });
+    if (state) {
+      characteristic.startNotifications();
+    } else {
+      characteristic.stopNotifications();
+    }
+  }
+
+  async requestMtu(expectedMtu) {
+    return 104; // mac
+  }
+
+  async readValue({ serviceId, characteristicId }) {
+    const characteristic = await getCharacteristic(this._connectGatt, { serviceId, characteristicId });
+    characteristic.readValue(); // then
+  }
+
+  async writeValue({ serviceId, characteristicId }, valueArray) {
+    const characteristic = await getCharacteristic(this._connectGatt, { serviceId, characteristicId });
+    await characteristic.writeValue(valueArray);
   }
 }
