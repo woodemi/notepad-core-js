38ecf10e3e686162c05eea25f02edbe1f667d13c
Jing Hong Gang <honggangjing@gmail.com>
2020-05-17 12:52:11 +0800

    Support external api on web demo

--- example/web/pages/index.js
+++ example/web/pages/index.js
@@ -1,28 +1,270 @@
-import Head from "next/head";
+import Head from 'next/head';
+import React from 'react';
+import {NotepadConnector, NotepadMode} from 'notepad-core';
 
-let notepadConnector;
+let notepadConnector = new NotepadConnector();
+const AUTH_TOKEN = '';
+let notepadClient;
 
-import("notepad-core").then(({ NotepadConnector }) => {
-   notepadConnector = new NotepadConnector();
-});
-
-async function bindRequestDevice() {
+export default class Home extends React.Component {
+  async bindRequestDevice() {
     await notepadConnector.requestDevice();
-}
+  }
 
-export default function Home() {
+  insertResult(message) {
+    this.setState({
+      results: this.data.results.push(message)
+    });
+  }
+
+  bindConnect() {
+    notepadConnector.connect(this.data.scanResult, AUTH_TOKEN)
+      .then((client, ConnectionState) => {
+        notepadClient = client;
+        notepadClient.onReceiveNotePenPointers((NotePenPointers) => {
+          this.insertResult('接收实时笔迹:' + NotePenPointers);
+        });
+        notepadClient.onImportMemoProgress((progress) => {
+          this.insertResult('导入笔记进度:' + progress);
+        });
+        notepadClient.onUpgradeProgress((progress) => {
+          this.insertResult('升级设备进度:' + progress);
+        });
+        this.setData({
+          width: notepadClient.width,
+          height: notepadClient.height
+        });
+        this.insertResult('连接设备成功:' + ConnectionState);
+      })
+      .catch((err) => {
+        this.insertResult('连接设备失败:' + err);
+      });
+  }
+
+  bindDisconnect() {
+    notepadConnector.disconnect()
+      .then(() => {
+        this.insertResult('解除连接成功');
+      })
+      .catch((err) => {
+        this.insertResult('解除连接失败:' + err);
+      });
+  }
+
+  bindClaimAuth() {
+    notepadConnector.claimAuth()
+      .then(() => {
+        this.insertResult('绑定设备成功');
+      })
+      .catch((err) => {
+        this.insertResult('绑定设备失败:' + err);
+      });
+  }
+
+  bindDisclaimAuth() {
+    notepadConnector.disclaimAuth()
+      .then(() => {
+        this.insertResult('解除绑定成功');
+      })
+      .catch((err) => {
+        this.insertResult('解除绑定失败:' + err);
+      });
+  }
+
+  bindSetMode() {
+    notepadConnector.setMode(NotepadMode.Sync)
+      .then(() => {
+        this.insertResult('设置Mode成功');
+      })
+      .catch((err) => {
+        this.insertResult('设置Mode失败:' + err);
+      });
+  }
+
+  bindGetMemoSummary() {
+    notepadConnector.getMemoSummary()
+      .then((memoSummary) => {
+        this.insertResult('获取队列摘要成功:' + memoSummary);
+      })
+      .catch((err) => {
+        this.insertResult('获取队列摘要失败:' + err);
+      });
+  }
+
+  bindImportMemo() {
+    notepadConnector.importMemo()
+      .then(() => {
+        this.insertResult('导入单个离线笔迹成功');
+      })
+      .catch((err) => {
+        this.insertResult('导入单个离线笔迹失败:' + err);
+      });
+  }
+
+  bindDeleteMemo() {
+    notepadConnector.deleteMemo()
+      .then(() => {
+        this.insertResult('删除单个离线笔迹成功');
+      })
+      .catch((err) => {
+        this.insertResult('删除单个离线笔迹失败:' + err);
+      });
+  }
+
+  bindGetDeviceName() {
+    notepadConnector.getDeviceName()
+      .then((deviceName) => {
+        this.insertResult('获取设备名称成功:' + deviceName);
+      })
+      .catch((err) => {
+        this.insertResult('获取设备名称失败:' + err);
+      });
+  }
+
+  bindDeviceNameInput(ev) {
+    this.setData({
+      deviceName: ev.target.value
+    });
+  }
+
+  bindSetDeviceName() {
+    notepadConnector.setDeviceName(this.data.deviceName)
+      .then(() => {
+        this.insertResult('设置设备名称成功');
+      })
+      .catch((err) => {
+        this.insertResult('设置设备名称失败:' + err);
+      });
+  }
+
+  bindGetBatteryInfo() {
+    notepadConnector.getBatteryInfo()
+      .then((batteryInfo) => {
+        this.insertResult('获取电量信息成功:' + batteryInfo);
+      })
+      .catch((err) => {
+        this.insertResult('获取电量信息失败:' + err);
+      });
+  }
+
+  bindGetDeviceDate() {
+    notepadConnector.getDeviceDate()
+      .then((deviceDate) => {
+        this.insertResult('获取设备时钟成功:' + deviceDate);
+      })
+      .catch((err) => {
+        this.insertResult('获取设备时钟失败:' + err);
+      });
+  }
+
+  bindDeviceDateInput(ev) {
+    this.setData({
+      deviceDate: ev.target.value
+    });
+  }
+
+  bindSetDeviceDate() {
+    notepadConnector.setDeviceDate(this.data.deviceDate)
+      .then(() => {
+        this.insertResult('设置设备时钟成功');
+      })
+      .catch((err) => {
+        this.insertResult('设置设备时钟失败:' + err);
+      });
+  }
+
+  bindGetAutoLockTime() {
+    notepadConnector.getAutoLockTime()
+      .then((autoLockTime) => {
+        this.insertResult('获取设备自动休眠时长成功:' + autoLockTime);
+      })
+      .catch((err) => {
+        this.insertResult('获取设备自动休眠时长失败:' + err);
+      });
+  }
+
+  bindAutoLockTimeInput(ev) {
+    this.setData({
+      autoLockTime: ev.target.autoLockTime
+    });
+  }
+
+  bindSetAutoLockTime() {
+    notepadConnector.setAutoLockTime(this.data.autoLockTime)
+      .then(() => {
+        this.insertResult('设置设备自动休眠时长成功');
+      })
+      .catch((err) => {
+        this.insertResult('设置设备自动休眠时长成功');
+      });
+  }
+
+
+  bindUpgradePathInput(ev) {
+    this.setData({
+      upgradePath: ev.target.value
+    });
+  }
+
+  bindUpgradeVersionInput(ev) {
+    this.setData({
+      upgradeVersion: ev.target.value
+    });
+  }
+
+  bindUpgrade() {
+    notepadConnector.upgrade(this.data.upgradePath, this.data.upgradeVersion)
+      .then(() => {
+        this.insertResult('设置设备自动休眠时长成功');
+      })
+      .catch((err) => {
+        this.insertResult('设置设备自动休眠时长成功');
+      });
+  }
+
+  render() {
+    let resultsView = [];
+    const results = this.data.results;
+    for (let i = 0; i < results.length; i++) {
+      resultsView.push(<li>{results[results.length - i]}</li>);
+    }
     return (
       <div className="container">
-          <Head>
-              <title>Notepad Demo</title>
-              <link rel="icon" href="/favicon.ico"/>
-          </Head>
-          <main>
-              <div className="grid">
-                  <button onClick={bindRequestDevice}>RequestDevice</button>
-              </div>
-          </main>
-          <style jsx>{`
+        <Head>
+          <title>Notepad Demo</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <main>
+          <div className="grid">
+            <button onClick={this.bindRequestDevice}>RequestDevice</button>
+            <button onClick={this.bindConnect}>Connect</button>
+            <button onClick={this.bindDisconnect}>Disconnect</button>
+            <button onClick={this.bindClaimAuth}>ClaimAuth</button>
+            <button onClick={this.bindDisclaimAuth}>DisclaimAuth</button>
+            <button onClick={this.bindSetMode}>SetMode</button>
+            <button onClick={this.bindGetMemoSummary}>GetMemoSummary</button>
+            <button onClick={this.bindImportMemo}>ImportMemo</button>
+            <button onClick={this.bindDeleteMemo}>DeleteMemo</button>
+            <button onClick={this.bindGetDeviceName}>GetDeviceName</button>
+                      设备名称：<input value={this.state.deviceName} onChange={this.bindDeviceNameInput}/>
+            <button onClick={this.bindSetDeviceName}>SetDeviceName</button>
+            <button onClick={this.bindGetBatteryInfo}>GetBatteryInfo</button>
+            <button onClick={this.bindGetDeviceDate}>GetDeviceDate</button>
+                      设备时钟（秒）：<input type="number" value={this.state.deviceDate} onChange={this.bindDeviceDateInput}/>
+            <button onClick={this.bindSetDeviceDate}>SetDeviceDate</button>
+            <button onClick={this.bindGetAutoLockTime}>GetAutoLockTime</button>
+                      休眠时长（分）：<input type="number" value={this.state.autoLockTime} onChange={this.bindAutoLockTimeInput}/>
+            <button onClick={this.bindSetAutoLockTime}>SetAutoLockTime</button>
+                      选择更新文件：<input type="file" value={this.state.upgradePath} onChange={this.bindUpgradePathInput}/>
+                      版本号：<input value={this.state.upgradeVersion} onChange={this.bindUpgradeVersionInput}/>
+            <button onClick={this.bindUpgrade}>Upgrade</button>
+                      笔记范围：
+                      W:{this.state.width || '未知'},H:{this.state.height || '未知'}
+                      操作结果：
+            <resultsView/>
+          </div>
+        </main>
+        <style jsx>{`
         .container {
           min-height: 100vh;
           padding: 0 0.5rem;
@@ -58,7 +300,7 @@ export default function Home() {
         }
       `}</style>
 
-          <style jsx global>{`
+        <style jsx global>{`
         html,
         body {
           padding: 0;
@@ -74,4 +316,5 @@ export default function Home() {
       `}</style>
       </div>
     );
+  }
 }
